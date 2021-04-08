37b2cd6162e3e628d5391f35b481c76933f592e8
Matías Salinas <matsalinas@uchile.cl>
2021-04-08 13:57:03 +0000

    Include i18n (react-intl)

--- src/Navbar/NavbarComponent.jsx
+++ src/Navbar/NavbarComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import {FormattedMessage} from 'react-intl';
 import './navbar.css';
 
 export const NavbarComponent = () => {
@@ -12,22 +13,47 @@ export const NavbarComponent = () => {
             expand="lg"
             className="mb-3 px-lg-5"
         >
-            <Navbar.Brand href="/">
+            <NavLink className="navbar-brand" to="/">
                 <img 
                     src="/images/logo-eol.svg" 
                     alt="Oficina de Educación Online Universidad de Chile" 
                     className="img-fluid"
                 />
-            </Navbar.Brand>
+            </NavLink>
 
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto">
-                    <NavLink className="nav-link" to="/home">Inicio</NavLink>
-                    <NavLink className="nav-link" to="/platform">Plataforma</NavLink>
-                    <NavLink className="nav-link" to="/projects">Proyectos</NavLink>
-                    <NavLink className="nav-link" to="/support">Apoyo Docente</NavLink>
-                    <NavLink className="nav-link" to="/oeo">Nosotros</NavLink>
+                    <NavLink className="nav-link" to="/home">
+                        <FormattedMessage
+                            id="navbar.home"
+                            defaultMessage="Inicio"
+                        />
+                    </NavLink>
+                    <NavLink className="nav-link" to="/platform">
+                        <FormattedMessage
+                            id="navbar.platform"
+                            defaultMessage="Plataforma"
+                        />
+                    </NavLink>
+                    <NavLink className="nav-link" to="/projects">
+                        <FormattedMessage
+                            id="navbar.projects"
+                            defaultMessage="Proyectos"
+                        />
+                    </NavLink>
+                    <NavLink className="nav-link" to="/support">
+                        <FormattedMessage
+                            id="navbar.teaching_support"
+                            defaultMessage="Apoyo Docente"
+                        />
+                    </NavLink>
+                    <NavLink className="nav-link" to="/oeo">
+                        <FormattedMessage
+                            id="navbar.we"
+                            defaultMessage="Nosotros"
+                        />
+                    </NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/index.js
+++ src/index.js
@@ -6,9 +6,23 @@ import './index.css';
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// i18n
+import { IntlProvider } from 'react-intl';
+import locale_en from "./translations/en.json";
+import locale_es from "./translations/es.json";
+
+const translation = {
+  'es': locale_es,
+  'en': locale_en
+};
+
+const language = navigator.language.split(/[-_]/)[0];
+
 ReactDOM.render(
   <React.StrictMode>
-    <MainApp />
+    <IntlProvider locale={language} messages={translation[language]}>
+      <MainApp />
+    </IntlProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
