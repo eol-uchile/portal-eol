760441515d2953370993fb27030b842fd705c283
Matías Salinas <madasaso@gmail.com>
2021-05-03 18:16:03 -0400

    Managed Platforms

--- src/Content/ManagedPlatforms/ManagedPlatformsComponent.js
+++ src/Content/ManagedPlatforms/ManagedPlatformsComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
 import "./platforms.css";
 
@@ -14,36 +14,64 @@ export const ManagedPlatformsComponent = () => {
                         </h2>
                     </div>
                     <div className="content-info col-md-6 my-auto" data-aos="fade-right" data-aos-duration="1000">
-                        <ListGroup className="mt-3">
-                            <a className="mb-3 shadow-lg text-left pl-2 rounded-right" href="https://eol.uchile.cl/" target="_blank" rel="noreferrer">
-                                <ListGroup.Item>
-                                    <FormattedMessage id="mp.eol.title" />
-                                </ListGroup.Item>
+                            <a className="mb-3" href="https://eol.uchile.cl/" target="_blank" rel="noreferrer">
+                                <Card>
+                                    <Card.Header>
+                                        <Card.Title>
+                                            <span className="toggle-arrow"><i className="fas fa-external-link-alt"></i></span>
+                                            <i className="fas fa-link"></i>
+                                            <FormattedMessage id="mp.eol.title" />
+                                        </Card.Title>
+                                    </Card.Header>
+                                </Card>
                             </a>
-                            <a className="mb-3 shadow-lg text-left pl-2 rounded-right" href="https://uabierta.uchile.cl/" target="_blank" rel="noreferrer">
-                                <ListGroup.Item>
-                                    <FormattedMessage id="mp.uabierta.title" />
-                                </ListGroup.Item>
+                            <a className="mb-3" href="https://uabierta.uchile.cl/" target="_blank" rel="noreferrer">
+                                <Card>
+                                    <Card.Header>
+                                        <Card.Title>
+                                            <span className="toggle-arrow"><i className="fas fa-external-link-alt"></i></span>
+                                            <i className="fas fa-link"></i>
+                                            <FormattedMessage id="mp.uabierta.title" />
+                                        </Card.Title>
+                                    </Card.Header>
+                                </Card>
                             </a>
-                            <a className="mb-3 shadow-lg text-left pl-2 rounded-right" href="https://cmmeduformacion.uchile.cl/" target="_blank" rel="noreferrer">
-                                <ListGroup.Item>
-                                    <FormattedMessage id="mp.cmmedu.title" />
-                                </ListGroup.Item>
+                            <a className="mb-3" href="https://cmmeduformacion.uchile.cl/" target="_blank" rel="noreferrer">
+                                <Card>
+                                    <Card.Header>
+                                        <Card.Title>
+                                            <span className="toggle-arrow"><i className="fas fa-external-link-alt"></i></span>
+                                            <i className="fas fa-link"></i>
+                                            <FormattedMessage id="mp.cmmedu.title" />
+                                        </Card.Title>
+                                    </Card.Header>
+                                </Card>
                             </a>
-                            <a className="mb-3 shadow-lg text-left pl-2 rounded-right" href="https://eduonline.ing.uchile.cl/" target="_blank" rel="noreferrer">
-                                <ListGroup.Item>
-                                    <FormattedMessage id="mp.eduonline.title" />
-                                </ListGroup.Item>
+                            <a className="mb-3" href="https://eduonline.ing.uchile.cl/" target="_blank" rel="noreferrer">
+                                <Card>
+                                    <Card.Header>
+                                        <Card.Title>
+                                            <span className="toggle-arrow"><i className="fas fa-external-link-alt"></i></span>
+                                            <i className="fas fa-link"></i>
+                                            <FormattedMessage id="mp.eduonline.title" />
+                                        </Card.Title>
+                                    </Card.Header>
+                                </Card>
                             </a>
-                            <a className="mb-3 shadow-lg text-left pl-2 rounded-right" href="https://virtual-labx.cl/" target="_blank" rel="noreferrer">
-                                <ListGroup.Item>
-                                    <FormattedMessage id="mp.virtuallabx.title" />
-                                </ListGroup.Item>
+                            <a className="mb-3" href="https://virtual-labx.cl/" target="_blank" rel="noreferrer">
+                                <Card>
+                                    <Card.Header>
+                                        <Card.Title>
+                                            <span className="toggle-arrow"><i className="fas fa-external-link-alt"></i></span>
+                                            <i className="fas fa-link"></i>
+                                            <FormattedMessage id="mp.virtuallabx.title" />
+                                        </Card.Title>
+                                    </Card.Header>
+                                </Card>
                             </a>
-                        </ListGroup>
                     </div>
                     <div className="content-info col-md-6 my-auto" data-aos="fade-left" data-aos-duration="1000">
-                        <img src="https://icountryschool.cl/wp-content/uploads/2020/08/placeholder.png" alt="platforms" className="img-fluid" />
+                        <img src="/images/platforms.png" alt="platforms" className="img-fluid" />
                     </div>
                 </div>
             </div>
