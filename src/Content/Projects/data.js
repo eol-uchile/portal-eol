b05e9d694caec046555944a97afc388eb43f85a1
Matías Salinas <madasaso@gmail.com>
2021-09-08 16:42:54 -0300

    Teams & Project fix images/texts

--- src/Content/Projects/data.js
+++ src/Content/Projects/data.js
@@ -5,9 +5,9 @@ export const projects_data = {
             'org'           : 'Oficina de Educación Online - Vicerrectoría de Tecnologías de la Información',
             'tier'          : 'Capacitación',
             'year'          : '2020 - 2021',
-            'description'   : 'Programa de actividades formativas que tiene por finalidad colaborar con la formación de académicas/as y ayudantes de la Universidad de Chile, en temas específicos de la enseñanza en línea.',
-            'courses'       : 'Herramientas de comunicación e interacción con el estudiante online (dictado el 2020), Herramientas para la evaluación en entornos virtuales de aprendizaje (dictado el 2020), Acompañamiento docente (dictándose) y Herramientas para ayudantes en la educación mediada por tecnologías (en conjunto con CEA Medicina, se dictará en 2021-S2)',
-            'participants'  : '313',
+            'description'   : 'Programa de actividades formativas que tiene por finalidad colaborar con la formación de académicos(as y ayudantes de la Universidad de Chile, en temas específicos de la enseñanza en línea.',
+            'courses'       : 'Herramientas de comunicación e interacción con el estudiante online (dictado el 2020), Herramientas para la evaluación en entornos virtuales de aprendizaje (dictado el 2020), Acompañamiento docente (dictándose) y Herramientas para ayudantes en la educación mediada por tecnologías (en conjunto con CEA Medicina, dictándose)',
+            'participants'  : '382',
             'image'         : '/images/projects/project1.png'
         },
 
@@ -15,10 +15,10 @@ export const projects_data = {
             'name'          : 'Economía',
             'org'           : 'Facultad de Ciencias Físicas y Matemáticas',
             'tier'          : 'Pregrado',
-            'year'          : 'versión 1 en 2020 (finalizada) y versión 2 en 2021 (dictándose)',
+            'year'          : 'versión 1 en 2020, versión 2 en 2021 (ambas finalizadas), versión 3 en 2021 (dictándose)',
             'description'   : 'Curso diseñado para ser impartido 100% online a estudiantes de la carrera de Ingeniería Industrial de la FCFM y que tiene como finalidad que el o la estudiante determine, por una parte, el propósito del estudio de las ciencias económicas y, por otra, su importancia, campo y método de acción.',
             'courses'       : '',
-            'participants'  : '859',
+            'participants'  : '1.495',
             'image'         : '/images/projects/project2.png'
         },
 		
@@ -26,10 +26,10 @@ export const projects_data = {
             'name'          : 'Cazadoras de estrellas',
             'org'           : 'Dirección de Vinculación Externa de la Facultad de Ciencias Físicas y Matemáticas',
             'tier'          : 'Proyecto interno',
-            'year'          : 'versión 1 en 2020, versión 2, 3 y 4 en 2021 (todas finalizadas), versión 5 en 2021 (dictándose) versión 6, 7 y 8 en 2021 (se dictarán en 2021-S2)',
+            'year'          : 'versión 1 en 2020, versión 2, 3, 4, 5, 6, 7 y 8 en 2021 (todas finalizadas)',
             'description'   : 'Proyecto de divulgación científica dirigido a niñas y jóvenes de enseñanza media de diferentes regiones del país; financiado por el Observatorio Europeo Austral ESO y dictado por un grupo de mujeres estudiantes de postgrado en Astronomía de la Universidad de Chile, con el apoyo de la Dirección de Vinculación Externa de la FCFM de la Universidad de Chile.',
-            'courses'       : 'Cazadoras de estrellas - Región de la Araucanía, Región de Coquimbo, Región de Magallanes y Antártica Chilena, Región de Tarapacá y Región de Los Ríos.',
-            'participants'  : '164',
+            'courses'       : 'Cazadoras de estrellas - Región de la Araucanía, Región de Coquimbo, Región de Magallanes y Antártica Chilena, Región de Tarapacá, Región de Los Ríos, Región de Antofagasta, Región del Biobío y Región Metropolitana.',
+            'participants'  : '297',
             'image'         : '/images/projects/project3.png'
         },
 		
@@ -37,7 +37,7 @@ export const projects_data = {
             'name'          : 'English as a Medium of Instruction (EMI): planning and teaching your subject in english',
             'org'           : 'Departamento de Postgrado y Postítulo',
             'tier'          : 'Postgrado',
-            'year'          : 'versión 1 en 2020 (finalizada), versión 2 en 2021 (dictándose)',
+            'year'          : 'versión 1 en 2020, versión 2 en 2021 (ambas finalizadas)',
             'description'   : 'Curso orientado a todos los que estén interesados ​​en impartir sus cursos a través de la instrucción en inglés, basada en las últimas investigaciones en enseñanza y aprendizaje.',
             'courses'       : '',
             'participants'  : '38',
@@ -59,7 +59,7 @@ export const projects_data = {
             'name'          : 'Controversias en el manejo de los factores de riesgo cardiovascular: de la teoría a la práctica',
             'org'           : 'Corporación Grupo Chileno de Trabajo en Ateroesclerosis',
             'tier'          : 'Externo',
-            'year'          : 'versión 1 en 2020 (finalizada), versión 2 en 2021 (se dictará en 2021-S2)',
+            'year'          : 'versión 1 en 2020 (finalizada), versión 2 en 2021 (dictándose)',
             'description'   : 'Curso dictado por la Corporación Grupo Chileno de Trabajo en Ateroesclerosis, con la colaboración de la Facultad de Medicina y EOL. Dirigido a médicos especialistas y no especialistas, nutricionistas y otros profesionales de la salud que se desempeñen en el ámbito de la nutrición y/o riesgo cardiovascular.',
             'courses'       : '',
             'participants'  : '62',
@@ -70,10 +70,10 @@ export const projects_data = {
             'name'          : 'Introducción a la vida universitaria',
             'org'           : 'Departamento de Pregrado',
             'tier'          : 'Pregrado',
-            'year'          : 'versión 1 en 2019, versión 2 en 2020 (todas finalizadas) y versión 3 en 2021 (dictándose), para Universidad de Chile | versión 1 en 2020, versión 2 en 2021 (todas finalizadas), para Universidad de O’Higgins',
+            'year'          : 'versión 1 en 2019, versión 2 en 2020 (ambas finalizadas), versión 3 en 2021 (dictándose), para la Universidad de Chile | versión 1 en 2020, versión 2 en 2021 (ambas finalizadas), para la Universidad de O’Higgins',
             'description'   : 'Curso destinado a mechonas y mechones (estudiantes de primer año) de la Universidad de Chile, que tiene por objetivo que los estudiantes puedan familiarizarse con la cultura universitaria, de manera que cuenten con toda la información necesaria para enfrentar el primer año académico. Además, se realizó una adaptación del curso para los estudiantes de primer año de la Universidad de O’Higgins.',
             'courses'       : '',
-            'participants'  : '15251',
+            'participants'  : '15.251',
             'image'         : '/images/projects/project7.png'
         },
 		
@@ -92,10 +92,10 @@ export const projects_data = {
             'name'          : 'Módulo integrador del ciclo pre-profesional',
             'org'           : 'Facultad de Ciencias Veterinarias y Pecuarias',
             'tier'          : 'Pregrado',
-            'year'          : 'versión 1 en 2020, versión 2 en 2020-2021 (todas finalizadas), versión 3 en 2021 (se dictará en 2021-S2)',
+            'year'          : 'versión 1 y versión 2 en 2020 (ambas finalizadas), versión 3 en 2021 (dictándose)',
             'description'   : 'Asignatura ubicada en el ciclo pre-profesional en el octavo semestre de la carrera de Medicina Veterinaria. Su objetivo principal es que los estudiantes logren integrar los aprendizajes adquiridos en todas las asignaturas previas a esta etapa, mediante la formulación de una propuesta de investigación relacionada con la disciplina.',
             'courses'       : '',
-            'participants'  : '223',
+            'participants'  : '419',
             'image'         : '/images/projects/project9.png'
         },
 		
@@ -103,10 +103,10 @@ export const projects_data = {
             'name'          : 'Inducción para el enfrentamiento de la pandemia por SARS-CoV-2',
             'org'           : 'Facultad de Medicina',
             'tier'          : 'Extensión',
-            'year'          : 'versión 1 y 2 en 2020, versión 3 en 2021 (todas finalizadas), para la Facultad de Medicina | versión 1 en 2020 (finalizada) para la Facultad de Odontología | versión 1 en 2020 (finalizada) para la Universidad de Los Lagos',
+            'year'          : 'versión 1 y 2 en 2020, versión 3 en 2021 (todas finalizadas), para la Facultad de Medicina | versión 1 en 2020 (finalizada), para la Facultad de Odontología | versión 1 en 2020 (finalizada), para la Universidad de Los Lagos',
             'description'   : 'Este curso permitirá a los internos(as) de la carreras adscritas a la Facultad de Medicina de la Universidad de Chile analizar, desde el punto de vista del conocimiento, aspectos relacionados con la pandemia por SARS-CoV-2 que está viviendo la población a nivel mundial y nacional. A través del aprendizaje significativo cada estudiante logrará conocimiento del SARS-CoV-2, medidas de autocuidado y manejo hospitalario inicial de la infección, todos aspectos relacionados a las condiciones emergentes que afectan a la población en Chile hoy. Además, se realizó una adaptación del curso para los estudiantes de la Facultad de Odontología y la Universidad de Los Lagos.',
             'courses'       : '',
-            'participants'  : '1748',
+            'participants'  : '1.748',
             'image'         : '/images/projects/project10.png'
         },
     ]
