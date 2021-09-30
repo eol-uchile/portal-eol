86a64925b1135e3d6bfd99b3a7e134a00a61a1fb
Matías Salinas <madasaso@gmail.com>
2021-09-30 12:17:05 -0300

    4 posts design

--- src/Content/Blog/ArticleComponent.js
+++ src/Content/Blog/ArticleComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { useFetchArticleData } from './ArticlesHooks';
 import { LanguageContext } from '../../Extras/Language/LanguageContext';
 import './articles.css';
+import { SeparatorTopComponent } from '../../Extras/Separators/SeparatorTopComponent';
 
 
 export const ArticleComponent = (props) => {
@@ -14,14 +15,14 @@ export const ArticleComponent = (props) => {
         <section id='article'>
             {
                 article.status === 'loading' ?
-                    <div className="d-flex justify-content-center">
+                    <div className="d-flex justify-content-center separator-margin">
                         <div className="spinner-border" role="status">
                             <span className="sr-only">Cargando...</span>
                         </div>
                     </div>
                 : 
                     article.status === 'success' ? (
-                        <div className="container" data-aos="fade-up" data-aos-duration="1000">
+                        <div className="container separator-margin" data-aos="fade-up" data-aos-duration="1000">
                             <Link className="goback" to='/blog'>
                                 <i class="fas fa-chevron-circle-left"></i>
                                 {" "}<FormattedMessage id="article.goback" />
@@ -29,6 +30,7 @@ export const ArticleComponent = (props) => {
                             <div className="row">
                                 <div className="col-12 post-info">
                                     <h2>{article.title}</h2>
+                                    <p class="h5">{article.description}</p>
                                     <p><i className="fas fa-user mr-2"></i>{" "}<FormattedMessage id="article.author" />: {article.author}</p>
                                     <p><i className="fas fa-tags"></i>{" "}<FormattedMessage id="article.tags" />: {" "}
                                         <span>{ article.tags?.join() }</span>
@@ -50,8 +52,9 @@ export const ArticleComponent = (props) => {
                         </div>
                     )
                     : 
-                        <h2 data-aos="fade-up" data-aos-duration="1000">404: <FormattedMessage id="article.notfound" /></h2>
+                        <h2 data-aos="fade-up" data-aos-duration="1000" className="separator-margin">404: <FormattedMessage id="article.notfound" /></h2>
             }
+            <SeparatorTopComponent />
         </section>
     )
 }
