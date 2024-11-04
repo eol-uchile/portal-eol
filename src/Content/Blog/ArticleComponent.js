e604fa3b57b05ae01bc31f50823a9a5a52dc8929
fsologureng <fsologur@uchile.cl>
2024-11-04 10:54:50 -0300

    refactor: changes some styles and assets.  * Removes some inline styles.  * Removes border embeded in a metaverso's post image.  * Removes non-comformant `<label>` tag from article title.  * Modifies logical test for complex author usage.

--- src/Content/Blog/ArticleComponent.js
+++ src/Content/Blog/ArticleComponent.js
@@ -8,7 +8,6 @@ import { SeparatorTopComponent } from '../../Extras/Separators/SeparatorTopCompo
 import { scrollToTop } from '../../Extras/ScrollToTopButton/scrolltop';
 
 
-
 export const ArticleComponent = (props) => {
     const { language } = useContext(LanguageContext);
 
@@ -32,11 +31,24 @@ export const ArticleComponent = (props) => {
                             </Link>
                             <div className="row">
                                 <div className="col-12 post-info">
-                                    <h2><label dangerouslySetInnerHTML={{__html: article.title}}></label></h2>
+                                    <h2 dangerouslySetInnerHTML={{__html: article.title}}></h2>
                                     <p className="h5 description" dangerouslySetInnerHTML={{__html: article.description}}></p>
 
                                     {
-                                        typeof article.author != 'string' ?
+                                        typeof article.author === 'string' ?
+                                        (
+                                            <>
+                                                <p><i className="fas fa-user mr-2"></i>{" "}<FormattedMessage id="article.author" />: {article.author}
+                                                </p>
+                                                <p>
+                                                    <i className="fas fa-tags"></i>{" "}<FormattedMessage id="article.tags" />:
+                                                    {article.tags &&
+                                                        article.tags.map(t => (<Link className="tag" to={`/blog/tags/${t}`}>{t}</Link>))
+                                                    }
+                                                </p>
+                                            </>
+                                        )
+                                        :
                                         (
                                             <div className="row d-flex justify-content-end px-3 my-4"  data-aos="zoom-in" data-aos-duration="1000">
                                                 <div className="col-12 col-md-6 d-flex align-items-center px-0 pr-3">
@@ -65,24 +77,7 @@ export const ArticleComponent = (props) => {
                                                 </div>
                                             </div> 
                                         )
-                                        :
-                                        (
-                                            <>
-                                                <p><i className="fas fa-user mr-2"></i>{" "}<FormattedMessage id="article.author" />: {article.author}
-                                                </p>
-                                                <p>
-                                                    <i className="fas fa-tags"></i>{" "}<FormattedMessage id="article.tags" />:
-                                                    {article.tags &&
-                                                        article.tags.map(t => (<Link className="tag" to={`/blog/tags/${t}`}>{t}</Link>))
-                                                    }
-                                                </p>
-                                            </>
-                                        )
                                     }
-                                    
-
-
-                                    
                                 </div>
                             </div>
                             <div className="row">
