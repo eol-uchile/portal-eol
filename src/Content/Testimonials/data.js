b3a8399395dc92766ca1cbf854fc6ee5a9f4920c
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-10-08 18:50:07 -0300

    feat: add testimonials with new users

--- src/Content/Testimonials/data.js
+++ src/Content/Testimonials/data.js
@@ -1,6 +1,30 @@
 export const testimonials_data = {
     'es' : [
         {
+            "name": 'Alicia Salomone',
+            "position": 'Directora de Relaciones Internacionales y Académica de la Facultad de Filosofía y Humanidades UCH',
+            "text": '(...) me di cuenta de que no era tan complejo como parecía, que se puede diseñar a través del programa Genially con mucha facilidad, presentaciones interactivas que ofrecen un montón de recursos, y luego también subir ese material a la plataforma no era complejo, era cuestión de aprender algunos mecanismos (...) a veces, tenemos el temor de que esto implique mucho trabajo, por un lado, y yo digo sí, es verdad, hay que trabajar un poco antes, pero luego el curso se desarrolla con mucha facilidad porque el material ya está preparado, entonces se reduce mucho la presión sobre el docente. (...) para mí la plataforma y el método que promueve la plataforma es excelente y no me canso de recomendársela a mis colegas y sobre todo de que comprendan que tienen que vencer la resistencia que todos tenemos siempre a cambiar nuestros hábitos, pero siempre los impulso a que lo hagan.',
+            "img": '/images/testimonials/05_testimonios_AliciaSalomone.jpg'
+        },
+        {
+            "name": 'Rodrigo Pascual',
+            "position": 'Académico del Departamento de Ingeniería Mecánica de la Facultad de Ciencias Físicas y Matemáticas UCH' ,
+            "text": '(...) Es potente porque ya las herramientas están probadas, las formas de evaluación sumativa y formativa han sido probadas. Entonces, eso ayuda a que lo que se haga sea de clase mundial. (...) Me gustó mucho el concepto porque creo en la clase invertida, en el flip classroom que le llaman, como medio para enriquecer los aprendizajes de los chicos. [...] Es una manera de implementar aprendizaje activo, donde el alumno va guiando su proceso de aprendizaje.',
+            "img": '/images/testimonials/06_testimonios_RodrigoPascual.jpg'
+        },
+        {
+            "name": 'Pamela Rojas',
+            "position": 'Coordinadora de capacitacion y onboarding (HRBP) de la Facultad de Economía de Negocios UCH' ,
+            "text": '(...) tuvimos que migrar a la inducción online, entonces, ahí llegamos a EOL, en donde tuvimos un trabajo muy dedicado con las personas de EOL en donde pudimos crear todo lo que nosotros queríamos en ese momento transmitirle los nuevos ingresos dentro de la facultad. Para nosotros, tener la educación en línea, en este caso para la inducción, es algo súper práctico, es algo súper bueno y yo creo que hay que seguir avanzando hacia eso, porque de hecho también nosotros vamos a seguir generando más cápsulas en temas que son muy administrativos que finalmente la idea es disminuir las preguntas de correo electrónico. Entonces, todo esto no ayuda enormemente.',
+            "img": '/images/testimonials/07_testimonios_PamelaRojas.jpg'
+        },
+        {
+            "name": 'Daniela Ejsmentewicz',
+            "position": 'Directora Ejecutiva del Programa de Reducción de Riesgos y Desastres (CITRID) UCH' ,
+            "text": 'Hemos realizado distintos cursos y en eso nos ha ayudado EOL, porque en la temática de reducción de riesgo de desastre es súper importante poder capacitar a la población sobre distintos temas y ese trabajo de capacitación lo hemos logrado hacer a través de la plataforma de EOL. Ya tenemos dos experiencias con EOL [...] la verdad fue muy bueno y los dos cursos fueron bastante masivos a nivel nacional. Se inscribieron personas de todas partes de Chile con distintos niveles educativos y todos pudieron acceder la plataforma, en general sin problemas, y fue una gran experiencia la verdad, porque te da muchas opciones para poder interactuar con los estudiantes y también la plataforma es bastante manejable por parte del docente una vez que aprendí a hacerlo.',
+            "img": '/images/testimonials/08_testimonios_DanielaEjsmentewicz .jpg'
+        },
+        {
             "name": 'Francisca Morales',
             "position": 'Coordinadora Académica, Programa de Inglés de Postgrado',
             "text": 'El trabajo con EOL ha sido excelente en todo ámbito y destaco el equipo altamente capacitado que me apoyó a través del ciclo completo de diseño, implementación y evaluación de nuestros talleres. En particular, agradezco al equipo de Diseño Instruccional que me apoyó enormemente en la adaptación y creación del contenido y a todos quienes ayudaron a crear un producto refinado de la más alta calidad.',
