2168178b55c19c0bba7cb12f1ca13ca4e7c665ea
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-11-12 11:39:29 -0300

    refactor: updates courses in teaching, adds effort hours to teach item and deletes, tier, org and year

--- src/Content/Teaching/TeachingItem.js
+++ src/Content/Teaching/TeachingItem.js
@@ -17,44 +17,21 @@ export const TeachingItem = ({teaching, side="img-left"}) => {
             <div className="col-md-7 col-lg-6 py-3 py-md-0" data-aos="fade-up" data-aos-duration="1000">
                 <h3 className="mb-4 text-left"><FormattedMessage id="teachings.teaching" />: {teaching.name}</h3>
                 <ul>
-                    { teaching.org && (
+                    { teaching.effort && (
                         <li>
                             <strong>
-                                <FormattedMessage id="teachings.org" />:
+                                <FormattedMessage id="teachings.effort" />:
                             </strong>
-                            {teaching.org}
+                            {teaching.effort}
                         </li>
                     ) }
-                    { teaching.tier && (
-                        <li>
-                            <strong>
-                                <FormattedMessage id="teachings.tier" />:
-                            </strong>
-                            {teaching.tier}
-                        </li>
-                    ) }
-                    { teaching.year && (
-                        <li>
-                            <strong>
-                                <FormattedMessage id="teachings.year" />:
-                            </strong>
-                            {teaching.year}
-                        </li>
-                    ) }
+                    
                     { teaching.description && (
                         <li>
                             <strong>
                                 <FormattedMessage id="teachings.description" />:
                             </strong>
                             {teaching.description}
-                        </li>
-                    ) }
-                    { teaching.participants && (
-                        <li>
-                            <strong>
-                                <FormattedMessage id="teachings.participants" />:
-                            </strong>
-                            {teaching.participants}
                         </li>
                     ) }
                     { teaching.link && (
